fa72882e4d0e4e8da5cbce4be9d86eccf60cc4a2
Marcos Alexandre <marcosxx1.ma@gmail.com>
2024-08-13 15:54:50 -0400

    feat: Better error handling

---
+++ loans/src/main/java/com/loans/commom/exception/ExceptionMessageUtils.java
@@ -0,0 +1,41 @@
+package com.loans.commom.exception;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Component;
+
+import static com.loans.constants.LoansConstants.MESSAGE_404;
+import static com.loans.constants.LoansConstants.RESOURCE_NOT_FOUND_WITH_DATA;
+
+
+@Component
+public class ExceptionMessageUtils {
+
+    private static MessageSourceAccessor staticMessageSourceAccessor;
+
+    private final MessageSourceAccessor messageSourceAccessor;
+
+    @Autowired
+    private ExceptionMessageUtils(MessageSourceAccessor messageSourceAccessor) {
+        this.messageSourceAccessor = messageSourceAccessor;
+    }
+
+    @PostConstruct
+    private void init() {
+        ExceptionMessageUtils.staticMessageSourceAccessor = this.messageSourceAccessor;
+    }
+
+    public static LoanAlreadyExistsException loanAlreadyExistsException() {
+        return new LoanAlreadyExistsException(staticMessageSourceAccessor.getMessage(MESSAGE_404));
+    }
+
+    public static IllegalArgumentException resourceNotFoundException(
+            String entityName, String fieldName, String fieldValue) {
+        String detailedMessage = String.format("%s not found with %s : '%s'", entityName, fieldName, fieldValue);
+
+        String message =
+                staticMessageSourceAccessor.getMessage(RESOURCE_NOT_FOUND_WITH_DATA, new Object[] {detailedMessage});
+        return new IllegalArgumentException(message);
+    }
+}
---
+++ loans/src/main/java/com/loans/constants/LoansConstants.java
@@ -0,0 +1,22 @@
+package com.loans.constants;
+
+public class LoansConstants {
+
+    private LoansConstants() {}
+
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "999 Av, Somewhere XY";
+
+    public static final String MESSAGE_200 = "response.success";
+    public static final String MESSAGE_201 = "account.creation.success";
+    public static final String MESSAGE_204 = "account.deletion.success";
+    public static final String MESSAGE_404 = "resource.not.found";
+    public static final String MESSAGE_500 = "error.general";
+
+    public static final String RESOURCE_NOT_FOUND_WITH_DATA = "resource.not.found-with-data";
+
+    public static final String  HOME_LOAN = "Home Loan"; // TODO - INTERNATIONALIZATION
+    public static final int  NEW_LOAN_LIMIT = 1_00_000; // TODO - INTERNATIONALIZATION
+    public static final String  MESSAGE_417_UPDATE= "Update operation failed. Please try again or contact Dev team"; // TODO - INTERNATIONALIZATION
+    public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team"; // TODO - INTERNATIONALIZATION
+}
