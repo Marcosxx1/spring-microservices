a5f097be819598f0bfd0685a3624d22c5956f600
marcos <marcosxx1.ma@gmail.com>
2024-08-22 20:44:22 -0400

    test: Fix CardServiceTest

--- cards/src/test/java/com/cards/service/CardServiceTest.java
+++ cards/src/test/java/com/cards/service/CardServiceTest.java
@@ -1,15 +1,22 @@
 package com.cards.service;
 
+import com.cards.commom.exception.ExceptionMessageUtils;
+import com.cards.constants.CardsConstants;
 import com.cards.domain.entity.Card;
 import com.cards.repository.CardRepository;
 import com.cards.service.CardServiceImpl;
 import com.cards.utils.CardUtils;
 import com.cards.commom.exception.CardAlreadyExistsException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.Optional;
 
@@ -17,29 +24,41 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CardServiceImpl.class, CardUtils.class})
 public class CardServiceTest {
+
+    @Autowired
+    private CardService cardService;
+
 
     @MockBean
     private CardRepository cardRepository;
 
     @MockBean
-    private CardUtils cardUtils;
-
-    @MockBean
     private MessageSourceAccessor messageSourceAccessor;
 
-     private CardServiceImpl cardService;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        messageSourceAccessor = mock(MessageSourceAccessor.class);
+        ReflectionTestUtils.setField(ExceptionMessageUtils.class, "staticMessageSourceAccessor", messageSourceAccessor);
+    }
+
 
     @Test
     public void testCreateCard_whenCardAlreadyExists_thenThrowCardAlreadyExistsException() {
         String mobileNumber = "987654321";
+        String errorMessage = "resource.already.exists";
+
 
          when(cardRepository.findByMobileNumber(mobileNumber)).thenReturn(Optional.of(new Card()));
+        when(messageSourceAccessor.getMessage(CardsConstants.RESOURCE_ALREADY_EXISTS))
+                .thenReturn(errorMessage);
 
-         cardService = new CardServiceImpl(cardRepository, cardUtils);
+        CardAlreadyExistsException thrown = assertThrows(CardAlreadyExistsException.class, () -> {
+            cardService.createCard(mobileNumber);
+        });
 
-         assertThrows(CardAlreadyExistsException.class, () -> cardService.createCard(mobileNumber));
-
-         verify(cardRepository, never()).save(any(Card.class));
+        assert (thrown.getMessage()).equals(errorMessage);
     }
 }
